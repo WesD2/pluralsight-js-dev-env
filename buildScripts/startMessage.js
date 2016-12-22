f65fd727ca73b4b0d5a7bdb4b09325bd0400815d
wes d2 <wesd2@sbcglobal.net>
2016-12-21 18:37:43 -0600

    module 9 work in progress

---
+++ buildScripts/startMessage.js
@@ -0,0 +1,4 @@
+//var chalk =require('chalk'); // es5 module syntax
+import chalk from 'chalk';  // es6 module syntax, uses babel to transpilie
+
+console.log(chalk.green('Starting app in dev mode...')); // eslint-disable-line no-console
